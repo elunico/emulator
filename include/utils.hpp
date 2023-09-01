f34270fc642c9a3efbf9ef347cbe6c95673ba1ae
Thomas Povinelli <elunico@users.noreply.github.com>
2023-09-01 14:18:05 -0400

    Fixed include issue

--- include/utils.hpp
+++ include/utils.hpp
@@ -15,7 +15,7 @@ void
 bytes_dump(emulator::byte* start, emulator::byte* end);
 
 std::vector<emulator::byte>
-load_binary_file(std::string_view filename);
+load_binary_file(std::string const& filename);
 
 bool
 skip_whitespace(std::ifstream& f);
@@ -24,7 +24,7 @@ bool
 skip_comment(std::ifstream& f);
 
 std::map<std::string, emulator::u64>
-parse_program_spec(std::string_view config_name);
+parse_program_spec(std::string const& config_name);
 
 void
 run_program_spec(std::string_view config_name, emulator::cpu& oncpu);
